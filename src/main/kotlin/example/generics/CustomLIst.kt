22c55043d17fc1848534e8b5705d26ee87d876b0
mogi86 <nobuyuki.mogi@gmail.com>
2021-01-10 15:47:06 +0900

    update sample code of Generics

--- src/main/kotlin/example/generics/CustomLIst.kt
+++ src/main/kotlin/example/generics/CustomLIst.kt
@@ -1,7 +1,11 @@
 package example.generics
 
+import java.util.function.BiPredicate
+
 // see: https://kotlinlang.org/docs/reference/generics.html
 
+// "<T>" is the definition of type parameter.
+// Type parameter is used for the receiver and the type for returning.
 fun <T> List<T>.implode(separator: String): String {
     var str = ""
 
@@ -11,4 +15,13 @@ fun <T> List<T>.implode(separator: String): String {
     }
 
     return str
+}
+
+fun <T> List<T>.filter2(predicate: (T) -> Boolean): List<T> {
+    val list = mutableListOf<T>()
+    for (v in this) {
+        if (predicate(v)) list.add(v)
+    }
+
+    return list
 }
--- src/test/kotlin/example/generics/CustomLIstTest.kt
+++ src/test/kotlin/example/generics/CustomLIstTest.kt
@@ -7,11 +7,21 @@ import org.junit.jupiter.api.Assertions.*
 internal class CustomLIstTest {
 
     @Test
-    fun listToString() {
+    fun implode() {
         val list = listOf("apple", "orange", "peach")
 
         val expected = "apple,orange,peach"
         val result = list.implode(",")
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun filter2() {
+        val list = listOf(1, 3, 5, 7, 9)
+
+        val expected = listOf(7, 9)
+        val result = list.filter2 { it > 5 }
 
         assertEquals(expected, result)
     }
